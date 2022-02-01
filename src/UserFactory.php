1e82001b72435559b102c3e275eafa0d45207c93
lrgdigidev <89582498+lrgdigidev@users.noreply.github.com>
2022-02-01 10:48:03 +0000

    Update UserFactory.php

--- src/UserFactory.php
+++ src/UserFactory.php
@@ -20,9 +20,9 @@ class UserFactory
         $user_map = config('azure-oath.user_map');
         $id_field = config('azure-oath.user_id_field');
 
-        $new_user = new $user_class;
+        /*$new_user = new $user_class;
         $new_user->$id_field = $azure_user->id;
-        
+
         if (config('azure-oath.email-verify-credentials', false)) {
             $new_user->email_verified_at = Carbon::now()->format('Y-m-d H:i:s');
         }
@@ -39,7 +39,30 @@ class UserFactory
 
         $new_user->save();
 
-        return $new_user;
+        return $new_user;*/
+
+        $azureUserEmail = $azure_user->email;
+
+        $userFields = [
+            $id_field => $azure_user->id
+        ];
+
+        if (config('azure-oath.email-verify-credentials', false)) {
+            $userFields['email_verified_at'] = Carbon::now()->format('Y-m-d H:i:s');
+        }
+
+        foreach($user_map as $azure_field => $user_field){
+            $userFields[$user_field] = $azure_user->$azure_field;
+        }
+
+        $user = $user_class::updateOrCreate(
+            [
+                'email' => $azureUserEmail
+            ],
+            $userFields
+        );
+
+        return $user;
     }
 
     public static function userCallback($callback)
